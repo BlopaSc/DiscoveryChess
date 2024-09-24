ccdd73f304717dd2fad7054e7821278ed7ce5ec7
BlopaSc <blopa.sauma@gmail.com>
2024-09-23 23:38:27 -0600

    Modified parser of data, added compressed datasets to repository using LFS, now allows loading games

--- extract_info.py
+++ extract_info.py
@@ -1,62 +1,73 @@
-import chess.pgn
-import json
+# Extracts information from the original .pgn files
+# Stores them as a compressed .bin files which can later be loaded
+
+# Conditional import, only required if parsing the original .pgn datasets
+if __name__ == "__main__":
+    import chess.pgn
+
 import os
+import time
+import zlib
 
-from os import listdir
-from os.path import isfile, join
+years = [' -1899', ' 1900-1949', ' 1950-1969', ' 1970-1989', ' 1990-1999', ' 2000-2004', ' 2005-2009', ' 2010-2014', ' 2015-2019', ' 2020', ' 2021', ' 2022', ' 2023']
 
-path = './Dataset'
-# files = [f for f in listdir(path) if isfile(join(path, f))]
-# years = [' -1899', ' 1900-1949', ' 1950-1969', ' 1970-1989', ' 1990-1999', ' 2000-2004', ' 2005-2009', ' 2010-2014', ' 2015-2019', ' 2020', ' 2021', ' 2022', ' 2023']
-years = [' 2000-2004', ' 2005-2009', ' 2010-2014', ' 2015-2019', ' 2020', ' 2021', ' 2022', ' 2023']
+def parse_original_sourcefiles():
+    stime = time.time()
+    for year in years:
+        games_list = []
+        output_path = f'./Dataset/dbLumbrasGigaBase{year}.bin'
+        file_path = f'./Dataset/LumbrasGigaBase{year}.pgn'
+        if not os.path.exists(file_path):
+            print("Error: Could not find file", file_path)
+            continue
+        if os.path.exists(output_path):
+            print("Skipping:", file_path, "output file already exists:", output_path)
+            continue
+        print("Parsing:", file_path)
+        with open(file_path) as pgn:
+            game_counter = 1
+            while True:
+                try:
+                    game = chess.pgn.read_game(pgn)
+                    if game is None:
+                        break
+                    game_info = {}
+                    game_info['Moves'] = []
+                    if game.headers['Result'] == '1-0':
+                        game_info['Winner'] = 'White'
+                    elif game.headers['Result'] == '0-1':
+                        game_info['Winner'] = 'Black'
+                    else:
+                        game_info['Winner'] = 'Draw'
+                    board = game.board()
+                    for move in game.mainline_moves():
+                        game_info['Moves'].append(board.san(move))
+                        board.push(move)
+                    games_list.append(game_info['Winner'][0] + ','.join(game_info['Moves']))
+                    game_counter += 1
+                except:
+                    print("utf-8 codec can't decode byte 0x80")
+                    continue
+            print(f"Read: {game_counter-1} games, Time taken: {time.time() - stime}")
+        with open(output_path, 'wb') as out:
+            print("Compressing at:", output_path)
+            out.write(zlib.compress(bytes('\n'.join(games_list), encoding='utf-8'), level=9))
+            print(f"Compressed successfully, Time taken: {time.time() - stime}")
 
-# print(files)
+def load_games():
+    games = {}
+    for year in years:
+        games_list = []
+        file_path = f'./Dataset/dbLumbrasGigaBase{year}.bin'
+        if not os.path.exists(file_path):
+            print("Failed to find file:", file_path)
+            continue
+        with open(file_path, 'rb') as binfile:
+            read_file = zlib.decompress(binfile.read()).decode('utf-8')
+            games_list = read_file.split('\n')
+            games[year[1:]] = [({'W': 1, 'B': -1, 'D': 0}[g[0]], g[1:]) for g in games_list]
+    return games
 
-# for file in files:
-for year in years:
-    games_list = []
-
-    output_dir = './Clean_Dataset/'
-    os.makedirs(output_dir, exist_ok=True)
-    # file_path = join(path, file)
-    file_path = f'./Dataset/LumbrasGigaBase{year}.pgn'
-    
-    with open(file_path) as pgn:
-        game_counter = 1
-        while True:
-            try:
-                game = chess.pgn.read_game(pgn)
-                
-                if game is None:
-                    break
-                print(game.headers)
-                game_info = {}
-                game_info['Moves'] = []
-
-                if game.headers['Result'] == '1-0':
-                    game_info['Winner'] = 'White'
-                elif game.headers['Result'] == '0-1':
-                    game_info['Winner'] = 'Black'
-                else:
-                    game_info['Winner'] = 'Draw'
-
-                board = game.board()
-
-                for move in game.mainline_moves():
-                    game_info['Moves'].append(board.san(move))  
-                    board.push(move)  
-
-                games_list.append(game_info)
-                
-                # print(f"Processed game {game_counter}")
-                game_counter += 1
-            except:
-                print("utf-8 codec can't decode byte 0x80")
-                continue
-    
-    output_file_path = os.path.join(output_dir, f'LumbrasGigaBase{year}.json')
-    
-    with open(output_file_path, "w") as output_file:
-        json.dump(games_list, output_file, indent=4)
-    print(f'Finished writing {output_file_path}')
-
+if __name__ == "__main__":
+    print("Parsing files")
+    parse_original_sourcefiles()
