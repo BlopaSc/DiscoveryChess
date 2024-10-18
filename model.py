0b2d62053dd9535102dce37107578c6bd5e1052d
Yi-Kai Lee <carllee0116@gmail.com>
2024-10-18 00:52:02 -0600

    Make CNN model parametize.

--- model.py
+++ model.py
@@ -2,23 +2,84 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNN_3layers(nn.Module):
+# class CNN_3layers(nn.Module):
+#     '''
+#     input tensor would be N*12*8*8
+#     state_vector: shape(12,)
+#     '''
+#     def __init__(self, state_vector_size):       
+#         super(CNN_3layers, self).__init__() 
+#         self.conv1 = nn.Conv2d(12, 32, kernel_size=3, stride=1, padding='same') 
+#         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding='same') 
+#         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding='same') 
+        
+#         # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=1, padding=1)
+#         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+#         self.dropout = nn.Dropout(0.5)
+        
+#         self.fc1 = nn.Linear(128*4*4 + state_vector_size, 256)
+#         self.fc2 = nn.Linear(256, 128)
+#         self.fc3 = nn.Linear(128, 1)
+    
+#     def forward(self, x, state_vector):
+#         '''
+#         input: x would be N*12*8*8
+#         output: N*1
+#         '''
+#         print(x.size())
+#         out = F.relu(self.conv1(x)) # Output N*8*8
+#         # out = self.pool1(out)
+#         # print(out.size())
+
+#         out = F.relu(self.conv2(out)) # Output N*8*8
+#         # out = self.pool1(out)
+#         # print(out.size)
+
+#         out = F.relu(self.conv3(out)) # Output N*4*4
+#         out = self.pool(out)
+#         # print(out.size)
+        
+#         out = out.view(out.size(0), -1) # Flatten for FC layer
+#         out = torch.cat((out, state_vector), dim=1)
+#         # print(out.size)
+        
+#         out = self.fc1(out)
+#         out = self.dropout(out)
+        
+#         out = self.fc2(out)
+#         out = self.dropout(out)
+        
+#         out = self.fc3(out)
+        
+#         return out
+    
+class CNN(nn.Module):
     '''
     input tensor would be N*12*8*8
     state_vector: shape(12,)
     '''
-    def __init__(self, state_vector_size):       
-        super(CNN_3layers, self).__init__() 
-        self.conv1 = nn.Conv2d(12, 32, kernel_size=3, stride=1, padding=1) 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1) 
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1) 
+    def __init__(self, input_channels, output_channels=[32, 64, 128], kernel_size=3, state_vector_size=12, num_layers=3):       
+        super(CNN, self).__init__() 
         
-        # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=1, padding=1)
+        self.convlayers = nn.ModuleList()
+        current_channels = input_channels
+        for out_channel in output_channels:
+            self.convlayers.append(
+                
+                nn.Conv2d(current_channels, out_channel, kernel_size=kernel_size, stride=1, padding='same')
+            )
+            current_channels = out_channel
+        
+        # Pooling layer for the output of the last Conv. layer
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+        # Dropout
         self.dropout = nn.Dropout(0.5)
         
-        self.fc1 = nn.Linear(128*4*4 + state_vector_size, 256)
+        # Fully Connected Layer
+        last_conv_out_size = output_channels[-1] * 4 * 4
+        
+        self.fc1 = nn.Linear(last_conv_out_size + state_vector_size, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 1)
     
@@ -27,29 +88,23 @@ class CNN_3layers(nn.Module):
         input: x would be N*12*8*8
         output: N*1
         '''
-        print(x.size())
-        out = F.relu(self.conv1(x)) # Output N*8*8
-        # out = self.pool1(out)
-        # print(out.size())
+        for conv in self.convlayers:
+            x = F.relu(conv(x))
+            print(x.size())
 
-        out = F.relu(self.conv2(out)) # Output N*8*8
-        # out = self.pool1(out)
-        # print(out.size)
-
-        out = F.relu(self.conv3(out)) # Output N*4*4
-        out = self.pool(out)
+        x = self.pool(x)
         # print(out.size)
         
-        out = out.view(out.size(0), -1) # Flatten for FC layer
-        out = torch.cat((out, state_vector), dim=1)
+        x = x.view(x.size(0), -1) # Flatten for FC layer
+        x = torch.cat((x, state_vector), dim=1)
         # print(out.size)
         
-        out = self.fc1(out)
-        out = self.dropout(out)
+        x = self.fc1(x)
+        x = self.dropout(x)
         
-        out = self.fc2(out)
-        out = self.dropout(out)
+        x = self.fc2(x)
+        x = self.dropout(x)
         
-        out = self.fc3(out)
+        x = self.fc3(x)
         
-        return out
+        return x
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from Chess import Chess
-from model import CNN_3layers
+from model import CNN
 
 
 game = Chess()
@@ -8,7 +8,7 @@ board_tensor, state_vector = game.to_tensor()
 board_tensor = board_tensor.unsqueeze(0)  # Shape: [1, 12, 8, 8]
 state_vector = state_vector.unsqueeze(0)  # Shape: [1, 22]
 
-model = CNN_3layers(state_vector_size=22)
+model = CNN(input_channels=12, output_channels=[32, 64, 128], kernel_size=3, state_vector_size=22, num_layers=3)
 
 output = model(board_tensor, state_vector)
 
